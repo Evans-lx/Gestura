898562fe9d11e6bea96e93ae69312efe087dde22
liuaitong@sjtu.edu.cn <liuaitong@sjtu.edu.cn>
2025-07-17 17:05:43 +0800

    code

---
+++ scripts/v1_5/eval/run_qa_egogesture.sh
@@ -0,0 +1,43 @@
+CKPT_NAME=""
+model_path="./checkpoints/${CKPT_NAME}"
+model_base="vicuna-7b-v1.5" #use in lora
+cache_dir="./cache_dir"
+GPT_Zero_Shot_QA="eval/egogesture"
+video_dir="egogesture"
+gt_file_question="${GPT_Zero_Shot_QA}/egogesture_open_test_q_lite.json" 
+gt_file_answers="${GPT_Zero_Shot_QA}/egogesture_open_test_a_lite.json" 
+output_dir="${GPT_Zero_Shot_QA}/${CKPT_NAME}_open"
+
+
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+  CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python3 llava/eval/video/run_inference_video_qa.py \
+      --model_path ${model_path} \
+      --cache_dir ${cache_dir} \
+      --video_dir ${video_dir} \
+      --gt_file_question ${gt_file_question} \
+      --gt_file_answers ${gt_file_answers} \
+      --output_dir ${output_dir} \
+      --output_name ${CHUNKS}_${IDX} \
+      --num_chunks $CHUNKS \
+      --chunk_idx $IDX &
+done
+
+wait
+
+output_file=${output_dir}/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ${output_dir}/${CHUNKS}_${IDX}.json >> "$output_file"
+done
+
---
+++ scripts/v1_5/pretrain.sh
@@ -0,0 +1,40 @@
+
+JSON_FOLDER=""
+IMAGE_FOLDER="./"
+VIDEO_FOLDER=""
+
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero2.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version v1 \
+    --data_path ${JSON_FOLDER}/*.json\
+    --image_folder ${IMAGE_FOLDER} \
+    --image_tower LanguageBind/LanguageBind_Image  \
+    --video_folder ${VIDEO_FOLDER} \
+    --video_tower LanguageBind/LanguageBind_Video_merge \
+    --mm_projector_type mlp2x_gelu \
+    --tune_mm_mlp_adapter True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir ./checkpoints/llava-7b-pretrain-vicuna1.5 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 24000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-3 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 4096 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 8 \
+    --lazy_preprocess True \
+    --report_to tensorboard \
